2997bce7f36c24496df7b8168ab30c4f48a12f02
zakjakub <mail@jakubzak.eu>
2021-01-09 17:02:49 +0100

    Some changes.

--- src/ChemieZijePlugin.php
+++ src/ChemieZijePlugin.php
@@ -17,8 +17,8 @@ class ChemieZijePlugin
 
     final public function loadCarbonFields(): void
     {
-        define('Carbon_Fields\DIR', get_parent_theme_file_path('vendor/htmlburger/carbon-fields'));
-        define('Carbon_Fields\URL', get_parent_theme_file_path('vendor/htmlburger/carbon-fields/'));
+        define('Carbon_Fields\DIR', 'vendor/htmlburger/carbon-fields');
+        define('Carbon_Fields\URL', 'vendor/htmlburger/carbon-fields/');
         Carbon_Fields::boot();
     }
 
