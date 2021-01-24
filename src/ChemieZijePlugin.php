0fe0a48034ffc92d48c73008576ced36d0bd2a73
zakjakub <mail@jakubzak.eu>
2021-01-24 22:26:00 +0100

    Some changes.

--- src/ChemieZijePlugin.php
+++ src/ChemieZijePlugin.php
@@ -16,8 +16,11 @@ class ChemieZijePlugin
         add_action('init', [$this, 'registerChemicalCalculationCategoryPost'], 0);
         add_action('init', [$this, 'registerChemicalIndustryMaterialPost'], 0);
         add_action('init', [$this, 'registerIndustrialChemistryFieldPost'], 0);
+        add_action('init', [$this, 'registerTeachingMaterialCategoryPost'], 0);
+        add_action('init', [$this, 'registerStudyMaterialCategoryPost'], 0);
         // Taxonomies
         add_action('init', [$this, 'registerContactSubDepartmentTaxonomy'], 0);
+        add_action('init', [$this, 'registerTeachingMaterialCategoryTypeTaxonomy'], 0);
         // Carbon fields
         add_action('after_setup_theme', [$this, 'loadCarbonFields']);
         add_action('carbon_fields_register_fields', [$this, 'registerPostFields']);
@@ -170,6 +173,108 @@ class ChemieZijePlugin
         register_post_type('calculation_category', $args);
     }
 
+    final public function registerTeachingMaterialCategoryPost(): void
+    {
+        $args = [
+            'label'               => __('teaching_material_categories'),
+            'description'         => __('Stránky o kategoriích výukových materiálů'),
+            'menu_icon'           => 'dashicons-welcome-learn-more',
+            'labels'              => [
+                'name'               => __('Kategorie výukových materiálů'),
+                'singular_name'      => __('Kategorie výukových materiálů'),
+                'menu_name'          => __('Kategorie výukových materiálů'),
+                'parent_item_colon'  => __('Nadřazená kategorie výukových materiálů'),
+                'all_items'          => __('Všechny kategorie výukových materiálů'),
+                'view_item'          => __('Zobrazit Kategorii výukových materiálů'),
+                'add_new_item'       => __('Přidat kategorii výukových materiálů'),
+                'add_new'            => __('Přidat novou kategorii výukových materiálů'),
+                'edit_item'          => __('Upravit kategorii výukových materiálů'),
+                'update_item'        => __('Aktualizovat kategorii výukových materiálů'),
+                'search_items'       => __('Vyhledat kategorii výukových materiálů'),
+                'not_found'          => __('Kategorie výukových materiálů nenalezena'),
+                'not_found_in_trash' => __('Nenalezeno v odstraněných kategoriích výukových materiálů'),
+            ],
+            'rewrite'             => [
+                'slug' => 'vyukove-materialy',
+            ],
+            'supports'            => [
+                'title',
+                'editor',
+                'author',
+                'thumbnail',
+                'excerpt',
+                'custom-fields',
+                'revisions',
+                'page-attributes',
+            ],
+            'public'              => true,
+            'hierarchical'        => false,
+            'show_ui'             => true,
+            'show_in_menu'        => true,
+            'show_in_nav_menus'   => true,
+            'show_in_admin_bar'   => true,
+            'has_archive'         => true,
+            'can_export'          => true,
+            'exclude_from_search' => false,
+            'yarpp_support'       => true,
+            'taxonomies'          => ['teach_mat_cat_type'],
+            'publicly_queryable'  => true,
+            'capability_type'     => 'page',
+        ];
+        register_post_type('teach_material_cat', $args);
+    }
+
+    final public function registerStudyMaterialCategoryPost(): void
+    {
+        $args = [
+            'label'               => __('study_material_categories'),
+            'description'         => __('Stránky o kategoriích studijních materiálů'),
+            'menu_icon'           => 'dashicons-welcome-learn-more',
+            'labels'              => [
+                'name'               => __('Kategorie studijních materiálů'),
+                'singular_name'      => __('Kategorie studijních materiálů'),
+                'menu_name'          => __('Kategorie studijních materiálů'),
+                'parent_item_colon'  => __('Nadřazená kategorie studijních materiálů'),
+                'all_items'          => __('Všechny kategorie studijních materiálů'),
+                'view_item'          => __('Zobrazit Kategorii studijních materiálů'),
+                'add_new_item'       => __('Přidat kategorii studijních materiálů'),
+                'add_new'            => __('Přidat novou kategorii studijních materiálů'),
+                'edit_item'          => __('Upravit kategorii studijních materiálů'),
+                'update_item'        => __('Aktualizovat kategorii studijních materiálů'),
+                'search_items'       => __('Vyhledat kategorii studijních materiálů'),
+                'not_found'          => __('Kategorie studijních materiálů nenalezena'),
+                'not_found_in_trash' => __('Nenalezeno v odstraněných kategoriích studijních materiálů'),
+            ],
+            'rewrite'             => [
+                'slug' => 'studijni-materialy',
+            ],
+            'supports'            => [
+                'title',
+                'editor',
+                'author',
+                'thumbnail',
+                'excerpt',
+                'custom-fields',
+                'revisions',
+                'page-attributes',
+            ],
+            'public'              => true,
+            'hierarchical'        => false,
+            'show_ui'             => true,
+            'show_in_menu'        => true,
+            'show_in_nav_menus'   => true,
+            'show_in_admin_bar'   => true,
+            'has_archive'         => true,
+            'can_export'          => true,
+            'exclude_from_search' => false,
+            'yarpp_support'       => true,
+            'taxonomies'          => [/*'contact_person'*/],
+            'publicly_queryable'  => true,
+            'capability_type'     => 'page',
+        ];
+        register_post_type('study_material_cat', $args);
+    }
+
     final public function registerChemicalIndustryMaterialPost(): void
     {
         $args = [
@@ -191,7 +296,7 @@ class ChemieZijePlugin
                 'not_found'          => __('Surovina chemického průmyslu nenalezena'),
                 'not_found_in_trash' => __('Nenalezeno v odstraněných surovinách chemického průmyslu'),
             ],
-            "rewrite" => [
+            "rewrite"             => [
                 "slug" => "material",
             ],
             'supports'            => [
@@ -294,6 +399,35 @@ class ChemieZijePlugin
                 'query_var'         => true,
                 'rewrite'           => ['slug' => 'type'],
                 'menu_icon'         => 'dashicons-groups',
+            ]
+        );
+    }
+
+    final public function registerTeachingMaterialCategoryTypeTaxonomy(): void
+    {
+        register_taxonomy(
+            'teach_mat_cat_type',
+            ['teach_material_cat', 'teach_material'],
+            [
+                'labels'            => [
+                    'name'              => _x('Typy výukových materiálů', 'taxonomy general name'),
+                    'singular_name'     => _x('Typ výukového materiálu', 'taxonomy singular name'),
+                    'search_items'      => __('Hledat typ výukového materiálu'),
+                    'all_items'         => __('Všechny typy výukových materiálů'),
+                    'parent_item'       => __('Nadřazený typ výukového materiálu'),
+                    'parent_item_colon' => __('Nadřazený typ výukového materiálu:'),
+                    'edit_item'         => __('Upravit typ výukového materiálu'),
+                    'update_item'       => __('Aktualizovat typ výukového materiálu'),
+                    'add_new_item'      => __('Přidat nový typ výukového materiálu'),
+                    'new_item_name'     => __('Název nového typu výukového materiálu'),
+                    'menu_name'         => __('Typy výukových materiálů'),
+                ],
+                'hierarchical'      => false,
+                'show_ui'           => true,
+                'show_admin_column' => true,
+                'query_var'         => true,
+                'rewrite'           => ['slug' => 'type'],
+                'menu_icon'         => 'dashicons-welcome-learn-more',
             ]
         );
     }
