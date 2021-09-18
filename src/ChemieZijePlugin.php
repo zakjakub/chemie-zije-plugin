4364fc8e4dd2f5d4c63d398b3ae2e5ed39027f43
Jakub Zak <mail@jakubzak.eu>
2021-09-18 16:32:45 +0200

    Removed field for formula.

--- src/ChemieZijePlugin.php
+++ src/ChemieZijePlugin.php
@@ -140,8 +140,7 @@ class ChemieZijePlugin
         $industryMaterialFields = Container::make('post_meta', 'Nastavení suroviny');
         $industryMaterialFields->where('post_type', '=', 'industry_material');
         $industryMaterialFields->add_fields([
-            Field::make('rich_text', 'formula', 'Chemický vzorec')->set_width(50),
-            Field::make('image', 'material_image', 'Obrázek suroviny')->set_width(50),
+            Field::make('image', 'material_image', 'Obrázek suroviny'),
         ]);
     }
 
